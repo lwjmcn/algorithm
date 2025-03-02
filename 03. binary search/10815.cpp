eb91d11fe6eb01e7d9a6fdc611101a33e8b89607
lwjmcn <clarecse02@gmail.com>
2025-03-02 17:39:55 +0900

    binary search 4: fix

--- 03. binary search/10815.cpp
+++ 03. binary search/10815.cpp
@@ -5,36 +5,21 @@ using namespace std;
 // easyyyy
 // 50만개에 대해 50만개 서치하기 -> 이분탐색
 // 라는 아이디어를 도출하는 게 중요
+
+// C++ <algorithm>에 binary_search 함수가 있다
+// 참고하자...
+// binary_search(arr, arr+N, numToFind);
+
 int n;
 int card[500005];
 
-bool isExist(int num)
-{
-  int left_idx = 0;
-  int right_idx = n - 1;
-  while (left_idx <= right_idx)
-  {
-    int mid = (left_idx + right_idx) / 2;
-    if (card[mid] == num)
-      return true;
-    else if (card[mid] > num)
-    {
-      right_idx = mid - 1;
-    }
-    else
-    {
-      left_idx = mid + 1;
-    }
-  }
-  return false;
-}
 int main()
 {
   std::ios::sync_with_stdio(false);
   cin.tie(0);
   cout.tie(0);
 
-  // freopen("../input.txt", "r", stdin);
+  // freopen("../input/.txt", "r", stdin);
 
   int m, num;
   cin >> n;
@@ -49,7 +34,8 @@ int main()
   for (int i = 0; i < m; i++)
   {
     cin >> num;
-    cout << isExist(num) << " ";
+    // cout << isExist(num) << " ";
+    cout << binary_search(card, card + n, num) << " ";
   }
 
   // 채점 전 freopen 주석 처리 할 것
